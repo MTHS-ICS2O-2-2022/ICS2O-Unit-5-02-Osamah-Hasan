f1d00007489b42685bba82d9c96d03e00b7e9510
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-05-02 21:43:11 +0000

    update 0.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+  // input
+  const randomNumberP = Math.floor(Math.random() * 6) + 1
+  const randomNumberN = Math.floor(Math.random() * -6) + -1
+
+  // process
+  function result() {
+  buttonOnChecked = document.getElementById("Positive.").checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("answer").innerHTML =
+      "Your number is: " + randomNumberP + "."
+  } else {
+    document.getElementById("answer").innerHTML =
+      "Your number is: " + randomNumberN + "."
+  }
+}
